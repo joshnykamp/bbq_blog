ee7c404f4cd3361e35bd46c2736dec8653625b4f
Josh Nykamp <joshnykamp@gmail.com>
2017-04-25 16:37:01 -0700

    fixed error

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :bbq_blog, BbqBlog.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "mighty-savannah-68762.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
